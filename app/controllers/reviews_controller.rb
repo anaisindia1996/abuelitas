a3df9fae040a6be585c1eeeb99a4672e5ad1b793
Marving Moreton <moreton.marving@gmail.com>
2022-08-16 14:40:09 -0400

    user can create an experience - missing images cloudinary tho

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def create
+    @experience = Experience.find(params[:experience_id])
+    @review = Review.new(review_params)
+    @review.experience = @experience
+
+    if @review.save
+      redirect_to experience_path(@experience)
+    else
+      render "experiences/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
