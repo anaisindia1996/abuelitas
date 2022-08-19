69555b1e6af8b879dbb4828c7e9687acfa706fbe
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-19 14:16:42 -0400

    added star rating to new review

---
+++ app/javascript/controllers/star_rating_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import StarRating from "star-rating.js"
+
+export default class extends Controller {
+  connect() {
+    new StarRating(this.element)
+  }
+}
+
+// Connects to data-controller="star-rating"
