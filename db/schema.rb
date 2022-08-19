d644db59e852444b9dd1d75f8dd93179a22bfc68
Camila Contreras Barrera <camila1127@hotmail.ca>
2022-08-19 12:28:33 -0400

    changes in card showpage

--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_190426) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
